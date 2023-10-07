1bf61fce11c11594bb83a6747e36e0fcdfdca863
stefan <stefanmihairahlitchi@gmail.com>
2023-10-07 16:11:34 +0300

    Kanban Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DragDropModule} from "@angular/cdk/drag-drop";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+import {CdkDropList} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CdkDropList,
+      DragDropModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
